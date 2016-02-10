08d779c1ffa041ac86b8fc69bfd54164cd4d6fe2
JV Estolas <fortis201@gmail.com>
2016-02-09 21:05:55 -0800

    pushing my contribution from local machine. noticed the api testing page is missing.

---
+++ server/config/tokens.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ClientID: "cpaG2FFmanKWaXuFgttDCefvdS0qBGG4",
+  ClientSecret: "T7dkixwyjUDf6oqImQ9xCYY1xn75hpH1u95qxFaS",
+  ServerID: "lbyXqB_lCr8okFviP_qd17Ba-DPiYA9Gbqy8O-F8"
+}
